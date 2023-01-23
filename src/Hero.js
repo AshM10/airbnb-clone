8f65caef85d2c305ddb1af6674b23a64b9e2c395
Ash Moreno <ashterfuentes@gmail.com>
2023-01-23 13:00:44 -0600

    feat: finish hero section

--- src/Hero.js
+++ src/Hero.js
@@ -3,6 +3,17 @@ import "./Hero.css";
 
 export default function Hero() {
   return (
-    <img src={Grid} alt="Fun and mindful activities" className="--hero-image" />
+    <div className="Hero">
+      <img
+        src={Grid}
+        alt="Fun and mindful activities"
+        className="--hero-image"
+      />
+      <h1>Online Experiences</h1>
+      <p>
+        Join unique interactive activities led by one-of-a-kind hosts - all
+        without leaving home.
+      </p>
+    </div>
   );
 }
